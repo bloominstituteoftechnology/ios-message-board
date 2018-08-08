51a20230e4409088394bd3023599b561d028873e
Carolyn Lea <carolynrlea@gmail.com>
2018-08-08 15:40:16 -0600

    add MessageThread model

--- MessageBoard/MessageBoard/Model/MessageThread.swift
+++ MessageBoard/MessageBoard/Model/MessageThread.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class MessageThread: Codable, Equatable
+{
+    static func == (lhs: MessageThread, rhs: MessageThread) -> Bool
+    {
+       return lhs == rhs 
+    }
+    
+    let title: String
+    let identifier: String
+    var messages: [MessageThread.Message]
+    
+    init(title: String, identifier: String = UUID().uuidString, messages: [MessageThread.Message] = [])
+    {
+        self.title = title
+        self.identifier = identifier
+        self.messages = messages
+    }
+    
+    struct Message: Equatable, Codable
+    {
+        let text: String
+        let sender: String
+        let timestamp: Date
+        
+        init(text: String, sender: String, timestamp: Date = Date())
+        {
+            self.text = text
+            self.sender = sender
+            self.timestamp = timestamp
+        }
+    }
+    
+    
+}
