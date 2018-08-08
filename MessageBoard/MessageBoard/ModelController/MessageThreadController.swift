5285ef8e73231a9c26269dd864effd67a7ba64a1
Carolyn Lea <carolynrlea@gmail.com>
2018-08-08 17:03:49 -0600

    Add MessageThreadController

--- MessageBoard/MessageBoard/ModelController/MessageThreadController.swift
+++ MessageBoard/MessageBoard/ModelController/MessageThreadController.swift
@@ -7,3 +7,75 @@
 //
 
 import Foundation
+
+class MessageThreadController
+{
+    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    var messageThreads: [MessageThread] = []
+    
+    func createMessageThread(title: String, completion: @escaping (Error?) -> Void)
+    {
+       let messageThread = MessageThread(title: title)
+        let url = MessageThreadController.baseURL.appendingPathComponent(messageThread.identifier).appendingPathExtension("json")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(messageThread)
+        } catch {
+            NSLog("Unable to encode \(messageThread): \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error
+            {
+                NSLog("Error saving messageThread on server: \(error)")
+                completion(error)
+                return
+            }
+            completion(nil)
+        }
+        .resume()
+    }
+   
+    func createMessage(messageThread: MessageThread, text: String, sender: String, completion: @escaping (Error?) -> Void)
+    {
+        let aMessageThread = MessageThread.Message(text: text, sender: sender)
+        let url = MessageThreadController.baseURL.appendingPathComponent(messageThread.identifier).appendingPathComponent("messages").appendingPathExtension("json")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(messageThread)
+        } catch {
+            NSLog("Unable to encode \(messageThread): \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error
+            {
+                NSLog("Error saving messageThread on server: \(error)")
+                completion(error)
+                return
+            }
+            else
+            {
+                messageThread.messages.append(aMessageThread)
+            }
+            completion(nil)
+        }
+        .resume()
+    }
+    
+    
+    
+    
+    
+    
+}
