547d7961d96aa79ad496f379d1d1b0f988287b4c
Conner Alegre <cga0@pm.me>
2018-08-08 15:04:09 -0600

    complete the storyboards with constraints and corresponding base view controllers

---
+++ ios-message-board/ios-message-board/View Controllers/MessageDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MessageDetailViewController.swift
+//  ios-message-board
+//
+//  Created by Conner on 8/8/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func addMessage(_ sender: Any) {
+        
+    }
+    
+    // MARK: - Properties
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var textView: UITextView!
+}
