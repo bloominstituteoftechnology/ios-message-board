0205017a1e2e765002664472022e20c44205dfcc
Linh Bouniol <paulalinh89@gmail.com>
2018-08-08 16:22:59 -0700

    Created messageThreadDetailTableVC

--- Message Board/Message Board/View Controllers/MessageThreadsTableViewController.swift
+++ Message Board/Message Board/View Controllers/MessageThreadsTableViewController.swift
@@ -9,94 +9,58 @@
 import UIKit
 
 class MessageThreadsTableViewController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    var messageThreadController = MessageThreadController()
 
     // MARK: - Outlets/Actions
     
     @IBOutlet var textField: UITextField!
     
     @IBAction func textField(_ sender: Any) {
+        guard let title = textField.text, title.count > 0 else { return }
+        
+        messageThreadController.createMessageThread(withTitle: title) { (error) in
+            if let error = error {
+                NSLog("Error creating message thread: \(error)")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+    // MARK: - TableViewDataSource
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return messageThreadController.messageThreads.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath)
 
-        // Configure the cell...
+        let message = messageThreadController.messageThreads[indexPath.row]
+        
+        cell.textLabel?.text = message.title
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowMessageDetail" {
+            let threadDetailVC = segue.destination as? MessageThreadDetailTableViewController
+            threadDetailVC?.messageThreadController = messageThreadController
+            
+            guard let index = tableView.indexPathForSelectedRow?.row else { return }
+            let messageThread = messageThreadController.messageThreads[index]
+            threadDetailVC?.messageThread = messageThread
+        }
     }
-    */
+    
 
 }
