86bf0ebbd3ba2f0f707945a01473ea01e261ec83
Conner Alegre <cga0@pm.me>
2018-08-08 16:01:12 -0600

    setup message threads table view controller and pass message controller down to sub view controllers

--- ios-message-board/ios-message-board/View Controllers/MessageThreadsTableViewController.swift
+++ ios-message-board/ios-message-board/View Controllers/MessageThreadsTableViewController.swift
@@ -14,69 +14,44 @@ class MessageThreadsTableViewController: UITableViewController {
         super.viewDidLoad()
     }
 
-    // MARK: - Table view data source
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return messageThreadController.messageThreads.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ThreadCell", for: indexPath)
+        cell.textLabel?.text = messageThreadController.messageThreads[indexPath.row].title
 
         return cell
     }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowThreadSegue" {
+            if let vc = segue.destination as? MessageThreadDetailTableViewController {
+                vc.messageThreadController = messageThreadController
+                
+                if let indexPath = self.tableView.indexPathForSelectedRow {
+                    vc.messageThread = messageThreadController.messageThreads[indexPath.row]
+                }
+            }
+        }
     }
-    */
-    
+
     @IBAction func createThread(_ sender: Any) {
+        if let text = textField.text {
+            messageThreadController.createMessageThread(title: text) { (error) in
+
+                if let error = error {
+                    NSLog("Error creating a message thread: \(error)")
+                }
+
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
-    
+
     // MARK: - Properties
     @IBOutlet var textField: UITextField!
     var messageThreadController: MessageThreadController = MessageThreadController()
