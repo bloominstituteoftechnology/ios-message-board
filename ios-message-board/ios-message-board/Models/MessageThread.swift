722f9803c4b5e33d02d653d1e6b6a4bb9b1a92d7
Conner Alegre <cga0@pm.me>
2018-08-08 16:53:18 -0600

    fix issue with equatable

--- ios-message-board/ios-message-board/Models/MessageThread.swift
+++ ios-message-board/ios-message-board/Models/MessageThread.swift
@@ -10,19 +10,14 @@ import Foundation
 
 class MessageThread: Codable, Equatable {
     required init(from decoder: Decoder) throws {
-        
-        // 1
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
-        // 2
         let title = try container.decode(String.self, forKey: .title)
         let identifier = try container.decode(String.self, forKey: .identifier)
         let messagesDictionaries = try container.decodeIfPresent([String: Message].self, forKey: .messages)
         
-        // 3
         let messages = messagesDictionaries?.compactMap({ $0.value }) ?? []
         
-        // 4
         self.title = title
         self.identifier = identifier
         self.messages = messages
@@ -36,7 +31,7 @@ class MessageThread: Codable, Equatable {
     
     static func == (lhs: MessageThread, rhs: MessageThread) -> Bool {
         return lhs.identifier == rhs.identifier &&
-        lhs.title == lhs.identifier
+        lhs.title == rhs.title
     }
     
     let title: String
