74f1bd659e608a202cb1c8213eb8f97192884f57
William Bundy <williambundy@icloud.com>
2018-08-08 17:04:32 -0700

    API hooked up; working on UI controls

--- MessageBoard/MessageBoard/MessageBoard.swift
+++ MessageBoard/MessageBoard/MessageBoard.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+// The built in error types are more complicated than
+// I'd like... this'll be fine
+typealias ErrorString = String
+typealias CompletionHandler = (ErrorString?)->Void
+
 struct Message: Codable, Comparable
 {
 	var text:String
@@ -33,14 +38,96 @@ class MessageTopic: Codable, Equatable
 	var identifier:String = UUID().uuidString
 	var messages:[Message] = []
 
+	init(_ title:String = "")
+	{
+		self.title = title
+	}
+
+	required init(from decoder:Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		title = try container.decode(String.self, forKey:.title)
+		identifier = try container.decode(String.self, forKey:.identifier)
+
+		let dict = try container.decodeIfPresent([String:Message].self, forKey:.messages)
+		self.messages = dict?.compactMap({$0.value}) ?? []
+	}
+
 	static func ==(l:MessageTopic, r:MessageTopic) -> Bool
 	{
 		return l.identifier == r.identifier
 	}
 }
 
+enum App {
+	static let baseURL = URL(string:"https://lambda-message-board.firebaseio.com/")!
+	static var controller = MessageController()
+}
+
 class MessageController
 {
+	var topics:[MessageTopic] = []
 
+	func buildRequest(_ ids:[String], _ httpMethod:String, _ data:Data?=nil) -> URLRequest
+	{
+		var url = App.baseURL
+		for id in ids {
+			url.appendPathComponent(id)
+		}
+		url.appendPathExtension("json")
+		var req = URLRequest(url: url)
+		req.httpMethod = httpMethod
+		req.httpBody = data
+		return req
+	}
+
+	func pushTopic(_ topic:MessageTopic, _ completion: @escaping CompletionHandler)
+	{
+		var data:Data?
+		do {
+			let enc = JSONEncoder()
+			data = try enc.encode(topic)
+		} catch {
+			completion("Failed to encode topic")
+		}
+		let req = buildRequest([topic.identifier], "PUT", data)
+		URLSession.shared.dataTask(with: req) { (data, _, error) in
+			if let error = error {
+				NSLog("Error uploading topic: \(error)")
+				completion("Error putting topic")
+				return
+			}
+			completion(nil)
+		}.resume()
+	}
+
+	func fetchTopics(_ completion:@escaping CompletionHandler)
+	{
+		let req = buildRequest([], "GET")
+		URLSession.shared.dataTask(with: req) { data, _, error in
+			if let error = error {
+				NSLog("Error fetching data: \(error)")
+				completion("Error fetching data")
+				return
+			}
+
+			guard let data = data else {
+				NSLog("Fetching data: Data was nil!")
+				completion("Error fetching: data was nil")
+				return
+			}
+
+			do {
+				let dec = JSONDecoder()
+				let msg = try dec.decode([String:MessageTopic].self, from: data)
+				self.topics = Array(msg.values)
+				completion(nil)
+				return
+			} catch {
+				NSLog("Fetching: Failed to decode data!")
+				completion("Error fetching: couldn't decoding data")
+				return
+			}
+		}.resume()
+	}
 }
 
--- MessageBoard/MessageBoard/MessageViews.swift
+++ MessageBoard/MessageBoard/MessageViews.swift
@@ -7,3 +7,97 @@
 //
 
 import Foundation
+import UIKit
+
+class TopicListTVC:UITableViewController
+{
+	@IBOutlet weak var newThreadField: UITextField!
+
+	@IBAction func createNewThread(_ sender: Any)
+	{
+		guard let name = newThreadField.text, name != "" else {
+			return
+		}
+
+		let topic = MessageTopic(name)
+		App.controller.pushTopic(topic) { error in
+			if error != nil {
+				return
+			}
+
+			self.fetchData()
+		}
+	}
+
+	func fetchData()
+	{
+		App.controller.fetchTopics { (err) in
+			if err != nil {
+				return
+			}
+
+			DispatchQueue.main.async {
+				self.refreshControl?.endRefreshing()
+				self.tableView.reloadData()
+			}
+		}
+	}
+
+	override func viewDidLoad()
+	{
+		refreshControl = UIRefreshControl(frame: tableView.frame)
+		refreshControl?.addTarget(self, action: #selector(pullToRefresh(_ :)), for:.valueChanged)
+		//fetchData()
+	}
+
+	@IBAction func pullToRefresh(_ sender: Any?)
+	{
+		fetchData()
+	}
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+	{
+		return App.controller.topics.count
+	}
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+	{
+		let cell = tableView.dequeueReusableCell(withIdentifier: "TopicCell")!
+		cell.textLabel!.text = App.controller.topics[indexPath.row].title
+		return cell
+	}
+
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if let dest = segue.destination as? MessageListTVC {
+			dest.topic = App.controller.topics[tableView.indexPathForSelectedRow!.row]
+		}
+	}
+}
+
+class MessageListTVC:UITableViewController
+{
+	var topic:MessageTopic!
+
+	override func viewWillAppear(_ animated: Bool)
+	{
+
+	}
+
+	func uiReload()
+	{
+		DispatchQueue.main.async {
+			self.tableView.reloadData()
+		}
+	}
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+	{
+		return topic.messages.count
+	}
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+	{
+		let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell")!
+		cell.textLabel!.text = topic.messages[indexPath.row].text
+		return cell
+	}
+
+}
