f775815bf96aec4ab0c8313d04af52becbf1ab02
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-15 18:27:38 -0400

    Create storyboards and models

---
+++ Message Board/Message Board/Model/MessageThread.swift
@@ -0,0 +1,38 @@
+//
+//  MessageThread.swift
+//  Message Board
+//
+//  Created by Lisa Sampson on 8/15/18.
+//  Copyright © 2018 Lisa Sampson. All rights reserved.
+//
+
+import Foundation
+
+class MessageThread: Equatable, Codable {
+    
+    let title: String
+    let identifier: String
+    var messages: [MessageThread.Message]
+    
+    init(title: String, identifier: String = UUID().uuidString, messages: [MessageThread.Message] = []) {
+        self.title = title
+        self.identifier = identifier
+        self.messages = messages
+    }
+    
+    static func == (lhs: MessageThread, rhs: MessageThread) -> Bool {
+        return lhs.title == rhs.title && lhs.identifier == rhs.identifier && lhs.messages == rhs.messages
+    }
+    
+    struct Message: Equatable, Codable {
+        let text: String
+        let sender: String
+        let timestamp: Date
+        
+        init(text: String, sender: String, timestamp: Date = Date()) {
+            self.text = text
+            self.sender = sender
+            self.timestamp = timestamp
+        }
+    }
+}
---
+++ Message Board/Message Board/View Controllers/MessageDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MessageDetailViewController.swift
+//  Message Board
+//
+//  Created by Lisa Sampson on 8/15/18.
+//  Copyright © 2018 Lisa Sampson. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sendButtonWasTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageTextView: UITextView!
+    
+}
