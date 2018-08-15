8c830d2f6bacef41b448b372b960b0ff43ee7080
Iyin Raphael <iyinraphael@gmail.com>
2018-08-15 16:01:30 -0700

    initial commit

---
+++ Message Board/Message Board/View Controller/MessageDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MessageDetailViewController.swift
+//  Message Board
+//
+//  Created by Iyin Raphael on 8/15/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func Send(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+  
+
+}
---
+++ Message Board/Message Board/View Controller/MessageThreadDetailTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MessageThreadDetailTableViewController.swift
+//  Message Board
+//
+//  Created by Iyin Raphael on 8/15/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class MessageThreadDetailTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+
+   
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+  
+
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
+
+}
