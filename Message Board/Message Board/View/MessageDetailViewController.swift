ab8fda612242afc1ad1cc2fb920b02bf6324757b
Dillon McElhinney <dillon.mce@gmail.com>
2018-09-12 15:02:36 -0600

    Lay out views and add view controllers

---
+++ Message Board/Message Board/View/MessageDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MessageDetailViewController.swift
+//  Message Board
+//
+//  Created by Dillon McElhinney on 9/12/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    // MARK: - Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func semdMessage(_ sender: Any) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Message Board/Message Board/View/MessageThreadsTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MessageThreadsTableViewController.swift
+//  Message Board
+//
+//  Created by Dillon McElhinney on 9/12/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import UIKit
+
+class MessageThreadsTableViewController: UITableViewController {
+
+    @IBOutlet weak var messageThreadTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addNewMessageThread(_ sender: Any) {
+        
+    }
+    
+    
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
