892a18ee4e5ba36ed8dcf577e9e0bfd70b89328b
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2018-09-13 04:00:00 -0500

    Finished project

--- Message Board/Model + Model Controller/MessageThreadController.swift
+++ Message Board/Model + Model Controller/MessageThreadController.swift
@@ -12,10 +12,105 @@ class MessageThreadController {
     
     func createMessageThread(with title: String, completion: @escaping (Error?) -> (Void)) {
         let messageThread = MessageThread(title: title)
-        let url = MessageThreadController.baseURL?.appendingPathComponent(messageThread.identifier).appendingPathComponent("json")
-        print(url?.absoluteString)
+        
+        let url = MessageThreadController.baseURL.appendingPathComponent(messageThread.identifier).appendingPathComponent(".json")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.put.rawValue
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(messageThread)
+            
+            URLSession.shared.dataTask(with: request) { (data, _, error) in
+                if let error = error {
+                    NSLog("Error PUTing message thread title in Firebase: \(error)")
+                    completion(error)
+                }
+                
+                self.messageThreads.append(messageThread)
+                
+                NSLog("PUT successful")
+                completion(nil)
+                
+            }.resume()
+            
+        } catch {
+            fatalError("Error encoding message thread title: \(error)")
+        }
+        
+    }
+    
+    
+    func createMessage(in thread: MessageThread, text: String, sender: String, completion: @escaping (Error?) -> (Void)) {
+        let message = MessageThread.Message(text: text, sender: sender)
+        
+        let url = MessageThreadController.baseURL.appendingPathComponent(thread.identifier).appendingPathComponent("messages").appendingPathComponent(".json")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(message)
+            
+            URLSession.shared.dataTask(with: request) { (data, _, error) in
+                if let error = error {
+                    NSLog("Error POSTing message thread title in Firebase: \(error)")
+                    completion(error)
+                }
+                
+                thread.messages.append(message)
+                
+                NSLog("POST successful")
+                completion(nil)
+                
+            }.resume()
+            
+        } catch {
+            fatalError("Error encoding message: \(error)")
+        }
+    }
+    
+    
+    func fetchMessageThreads(completion: @escaping (Error?) -> (Void)) {
+        let url = MessageThreadController.baseURL.appendingPathComponent(".json")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        let _ = URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error GETing data from Firebase: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from Firebase API")
+                completion(NSError())
+                return
+            }
+            
+            let response = String(data: data, encoding: .utf8)
+            if response == "null" {
+                NSLog("No data to work with")
+                completion(nil)
+                return
+            }
+                
+            do {
+                let jsonDecoder = JSONDecoder()
+                let messageThreadDictionaries = try jsonDecoder.decode([String: MessageThread].self, from: data)
+                self.messageThreads = messageThreadDictionaries.map { $0.value }
+                completion(nil)
+            } catch {
+                NSLog("Unable to decode data: \(error)")
+                completion(error)
+                return
+            }
+            
+        }.resume()
     }
     
     var messageThreads: [MessageThread] = []
-    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")
+    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
 }
--- Message Board/View Controllers/MessageThreadsTableViewController.swift
+++ Message Board/View Controllers/MessageThreadsTableViewController.swift
@@ -8,90 +8,82 @@
 
 import UIKit
 
+enum ReuseIdentifiers: String {
+    case messageThreadCell = "ThreadCell"
+    case messageCell = "MessageCell"
+}
+
+enum SegueIdentifiers: String {
+    case toMessageThreadDetailsScene = "ShowMessageThreadDetails"
+    case toMessageDetailsScene = "ShowMessageDetails"
+    case toNewMessageScene = "ShowNewMessage"
+}
+
 class MessageThreadsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let refreshControl = UIRefreshControl()
+        tableView.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(fetchThreads(_:)), for: .valueChanged)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchThreads(self)
+        self.tableView.reloadData()
+    }
+    
+    @objc private func fetchThreads(_ sender: Any) {
+        messageThreadController.fetchMessageThreads { (error) -> (Void) in
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.tableView.refreshControl?.endRefreshing()
+            }
+        }
     }
 
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return messageThreadController.messageThreads.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: ReuseIdentifiers.messageThreadCell.rawValue, for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = messageThreadController.messageThreads[indexPath.row].title
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    // MARK:- Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == SegueIdentifiers.toMessageThreadDetailsScene.rawValue {
+            guard let destVC = segue.destination as? MessageThreadDetailTableViewController,
+                  let index = tableView.indexPathForSelectedRow?.row else { return }
+            destVC.messageThreadController = messageThreadController
+            destVC.messageThread = messageThreadController.messageThreads[index]
+        }
     }
-    */
     
-    @IBAction func textFieldAction(_ sender: Any) {
+    
+    // MARK:- IBActions
+    @IBAction func textFieldAction(_ sender: UITextField) {
+        guard let threadTitle = textField.text else { return }
         
+        messageThreadController.createMessageThread(with: threadTitle) { (error) -> (Void) in
+            self.fetchThreads(self)
+        }
     }
     
     
+    // MARK:- Properties
+    let messageThreadController = MessageThreadController()
+    
+    
+    // MARK:- IBOutlets
     @IBOutlet weak var textField: UITextField!
     
 }
