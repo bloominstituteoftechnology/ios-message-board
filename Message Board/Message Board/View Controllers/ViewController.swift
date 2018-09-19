fd66ba9d3b55df28e234387f37560b6653785adb
Scott Bennett <stt.btt@gmail.com>
2018-09-19 10:03:30 -0400

    Initial commit

---
+++ Message Board/Message Board/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Message Board
+//
+//  Created by Scott Bennett on 9/19/18.
+//  Copyright © 2018 Scott Bennett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
