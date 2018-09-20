447eb0ec667c19929ac0b1b865174fd47075b4d8
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-20 01:44:43 -0700

    model completed

---
+++ Message Board/Message Board/Model/MessageThread.swift
@@ -0,0 +1,32 @@
+//
+//  MessageThread.swift
+//  Message Board
+//
+//  Created by Welinkton on 9/20/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import Foundation
+
+class MessageThread: Codable {
+    let title: String
+    let identifier: String
+    var messages: [MessageThread.Message]
+    
+    struct Message {
+        let text: String
+        let sender: String
+        let timestamp: Date
+        
+        init(text:String, sender:String, timestamp:Date = Date()){
+            self.text = text
+            self.sender = sender
+            self.timestamp = timestamp
+        }
+    }
+    init(title: String, identifier: String, messages:[MessageThread.Message])
+        self.title = title
+        self.identifier = identifier
+        self.messages = messaages
+    
+}
