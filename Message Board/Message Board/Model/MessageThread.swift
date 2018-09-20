adfad63b3882c1f22a4c0140cb0f8e1181011e8b
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-20 07:21:17 -0700

    Conformed to Coddle  Protocol

--- Message Board/Message Board/Model/MessageThread.swift
+++ Message Board/Message Board/Model/MessageThread.swift
@@ -8,12 +8,17 @@
 
 import Foundation
 
-class MessageThread: Codable {
+class MessageThread: Codable, Equatable {
+    
+    static func == (lhs: MessageThread, rhs: MessageThread) -> Bool {
+        return lhs.title == rhs.title && lhs.identifier == rhs.identifier && lhs.messages == rhs.messages
+    }
+    
     let title: String
     let identifier: String
     var messages: [MessageThread.Message]
     
-    struct Message {
+    struct Message: Codable, Equatable {
         let text: String
         let sender: String
         let timestamp: Date
@@ -25,7 +30,7 @@ class MessageThread: Codable {
         }
     }
     
-    init(title: String, identifier: String = UUID().uuidString , messages: []){
+    init(title: String, identifier: String = UUID().uuidString , messages: [MessageThread.Message] ){
         self.title = title
         self.identifier = identifier
         self.messages = messages
