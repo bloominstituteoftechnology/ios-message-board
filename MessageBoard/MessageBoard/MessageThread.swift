c025a5beae7b79f04e83644e36355ed2b243daee
Paul Yi <yipaulc@gmail.com>
2018-10-23 11:57:35 -0700

    Create an initializer for the MessageThread class

---
+++ MessageBoard/MessageBoard/MessageThread.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+class MessageThread: Codable {
+    let title: String
+    let identifier: String
+    var messages: [MessageThread.Message]
+    
+    init(title: String, identifier: String = UUID().uuidString, messages: [MessageThread.Message] = []) {
+        <#statements#>
+    }
+    
+    struct Message: Equatable, Codable {
+        let text: String
+        let sender: String
+        let timestamp: Date
+        
+        init(text: String, sender: String, timestamp: Date = Date()) {
+            self.text = text
+            self.sender = sender
+            self.timestamp = timestamp
+        }
+    }
+    
+    
+}
