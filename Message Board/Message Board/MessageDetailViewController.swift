9cbe82fb7c10490ed2b32698fa2fa7077ea91b9f
Moses Robinson <mosesjrobinson@icloud.com>
2018-10-23 14:08:51 -0700

    storyboard complete

---
+++ Message Board/Message Board/MessageDetailViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func sendMessageButton(_ sender: Any) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
