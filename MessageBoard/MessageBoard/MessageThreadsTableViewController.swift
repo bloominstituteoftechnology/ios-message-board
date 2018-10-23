fcfc71675ef2cdad787359c60fd427e796882050
Paul Yi <yipaulc@gmail.com>
2018-10-23 14:23:03 -0700

    Implement the prepare(for segue: ...) method

--- MessageBoard/MessageBoard/MessageThreadsTableViewController.swift
+++ MessageBoard/MessageBoard/MessageThreadsTableViewController.swift
@@ -38,7 +38,13 @@ class MessageThreadsTableViewController: UITableViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        if segue.identifier == "AddEntry" {
+            guard let indexPath = tableView.indexPathForSelectedRow,
+                let detailTVC = segue.destination as? MessageThreadDetailTableViewController else { return }
+            let message = messageThreadController.messageThreads[indexPath.row]
+            detailTVC.messageThread = message
+            detailTVC.messageThreadController = messageThreadController
+        }
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
