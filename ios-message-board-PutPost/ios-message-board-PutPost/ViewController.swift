076424d519d4200ce4736eac0776dd5bca0a3a79
Rwoltx <richardx1016@gmail.com>
2018-10-23 21:40:15 -0400

    First part complete

---
+++ ios-message-board-PutPost/ios-message-board-PutPost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-message-board-PutPost
+//
+//  Created by Rick Wolter on 10/23/18.
+//  Copyright © 2018 RNWolter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
