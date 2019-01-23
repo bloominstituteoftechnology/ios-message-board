b8b96a3e9933422bba2c2c1042b509ab2497cff0
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-23 12:29:27 -0800

    Add message detail VC and view with send button and text elements connected.

---
+++ Message Board/Message Board/MessageDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MessageDetailViewController.swift
+//  Message Board
+//
+//  Created by Nathanael Youngren on 1/23/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func sendButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageTextView: UITextView!
+    
+}
