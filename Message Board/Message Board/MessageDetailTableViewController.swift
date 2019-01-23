b4b0b7cde4afcca006e594f260842b75c85392cb
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-23 12:20:40 -0800

    Add message detail TVC and view.

---
+++ Message Board/Message Board/MessageDetailTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MessageDetailTableViewController.swift
+//  Message Board
+//
+//  Created by Nathanael Youngren on 1/23/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath)
+
+        
+
+        return cell
+    }
+    
+
+}
