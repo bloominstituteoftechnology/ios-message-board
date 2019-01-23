c93c98c475c9ab6e250e619c5bdd61f70d417265
Frulwinn <frulwinn@gmail.com>
2019-01-23 15:12:05 -0800

    added funcs for tableView in MessageThreadsTVC

--- MessageBoard/MessageBoard/Controllers/MessageThreadsTVC.swift
+++ MessageBoard/MessageBoard/Controllers/MessageThreadsTVC.swift
@@ -5,6 +5,8 @@ import UIKit
 class MessageThreadsTVC: UITableViewController {
 
     // MARK: Properties
+    var messageThreadController = MessageThreadController()
+    
     @IBOutlet weak var testField: UITextField!
     
     @IBAction func textField(_ sender: Any) {
@@ -17,26 +19,18 @@ class MessageThreadsTVC: UITableViewController {
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return messageThreadController.messageThreads.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath)
 
-        // Configure the cell...
-
+        let messageThread = messageThreadController.messageThreads[indexPath.row]
+        cell.textLabel?.text = messageThread.title
+        cell.detailTextLabel?.text = messageThread.identifier
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
