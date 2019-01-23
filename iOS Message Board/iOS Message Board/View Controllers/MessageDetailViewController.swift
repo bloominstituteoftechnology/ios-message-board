30b25a5380139d5df3b2f8fa6d334bd3631f2c2e
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-01-23 13:33:45 -0800

    Storyboard Layout

---
+++ iOS Message Board/iOS Message Board/View Controllers/MessageDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MessageDetailViewController.swift
+//  iOS Message Board
+//
+//  Created by Jaspal on 1/23/19.
+//  Copyright © 2019 Jaspal Suri. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func sendMessage(_ sender: Any) {
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
