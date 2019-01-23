9f4ab0f9be14b791d010f3754bdbfdad71816e4c
Frulwinn <frulwinn@gmail.com>
2019-01-23 13:27:07 -0800

    added static func == to make MessageThread equatable

--- MessageBoard/MessageBoard/MessageThread.swift
+++ MessageBoard/MessageBoard/MessageThread.swift
@@ -3,10 +3,23 @@
 import Foundation
 
 
-class MessageThread: Codable {
+class MessageThread: Equatable, Codable {
+    
+    var messages: [MessageThread.Message]
     
     let title: String
     let identifier: String
+    
+    init(title: String, identifier: String = UUID().uuidString) {
+        self.title = title
+        self.identifier = identifier
+    }
+    
+    static func == (lhs: MessageThread, rhs: MessageThread) -> Bool {
+        return lhs.messages == rhs.messages &&
+        lhs.title == rhs.title &&
+        lhs.identifier == rhs.identifier
+    }
     
     struct Message: Equatable, Codable {
         let text: String
