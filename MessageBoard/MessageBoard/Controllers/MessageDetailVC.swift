8d964a9574eb7801774aca33930f6b6e3a0fc053
Frulwinn <frulwinn@gmail.com>
2019-01-23 12:50:44 -0800

    set up MessageDetailVC and hooked up outlets

---
+++ MessageBoard/MessageBoard/Controllers/MessageDetailVC.swift
@@ -0,0 +1,21 @@
+//  Copyright © 2019 Frulwinn. All rights reserved.
+
+import UIKit
+
+class MessageDetailVC: UIViewController {
+
+    //MARK: Properties
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func send(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
