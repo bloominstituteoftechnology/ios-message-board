af79a4ddbbf3dcdf4db988394704ff390e17e7c9
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-23 12:14:49 -0800

    Add main view and connect associated TVC.

---
+++ Message Board/Message Board/MessageThreadsTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MessageThreadsTableViewController.swift
+//  Message Board
+//
+//  Created by Nathanael Youngren on 1/23/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class MessageThreadsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ThreadCell", for: indexPath)
+
+
+        return cell
+    }
+    
+    @IBAction func textFieldSubmitted(_ sender: UITextField) {
+    }
+    
+    // Properties
+    
+    @IBOutlet weak var textField: UITextField!
+
+}
