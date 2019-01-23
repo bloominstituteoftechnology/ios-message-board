5312a5f1350a163f1c42603df9637af5f1d30439
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-23 15:11:44 -0500

    Add MessageDetailViewController and scene with outlets/actions

---
+++ MessageBoardApp/MessageBoardApp/MessageDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MessageBoardApp
+//
+//  Created by Nelson Gonzalez on 1/23/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var messaeTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func sendBarButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
+
