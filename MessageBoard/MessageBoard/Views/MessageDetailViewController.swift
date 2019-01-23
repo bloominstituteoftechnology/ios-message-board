cddaa710053594737100483377fdd93d65718b06
Angel <angelbuenrostro@outlook.com>
2019-01-23 15:59:09 -0500

    Add 3rd storyboard and custom view controller

---
+++ MessageBoard/MessageBoard/Views/MessageDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MessageDetailViewController.swift
+//  MessageBoard
+//
+//  Created by Angel Buenrostro on 1/23/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func sendButton(_ sender: UIBarButtonItem) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
