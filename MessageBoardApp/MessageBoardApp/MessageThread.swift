2d249e471fcf10d40a7c0a5eb76d6a5796b9db9c
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-23 15:31:43 -0500

    Add Model

---
+++ MessageBoardApp/MessageBoardApp/MessageThread.swift
@@ -0,0 +1,44 @@
+//
+//  MessageThread.swift
+//  MessageBoardApp
+//
+//  Created by Nelson Gonzalez on 1/23/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import Foundation
+
+
+class MessageThread: Equatable, Codable {
+    
+    
+    let title: String
+    let identifier: String
+    
+    var messages: [MessageThread.Message]
+    
+    init(title: String, identifier: String = UUID().uuidString, messages: [MessageThread.Message] = []) {
+        self.title = title
+        self.identifier = identifier
+        self.messages = messages
+    }
+    
+    struct Message: Equatable, Codable {
+        let text: String
+        let sender: String
+        let timestamp: Date
+        
+        init(text: String, sender: String, timestamp: Date = Date()) {
+            self.text = text
+            self.sender = sender
+            self.timestamp = timestamp
+        }
+    }
+    
+    //Why?
+    static func == (lhs: MessageThread, rhs: MessageThread) -> Bool {
+        return lhs.title == rhs.title && lhs.identifier == rhs.identifier && lhs.messages == rhs.messages
+    }
+}
+
+
