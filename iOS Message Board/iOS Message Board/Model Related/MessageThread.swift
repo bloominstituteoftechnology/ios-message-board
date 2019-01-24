ef818f1dc34fcc53a884530198d06012ccd25467
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-01-24 04:05:24 -0800

    checkpoint that should've been pushed yesterday

---
+++ iOS Message Board/iOS Message Board/Model Related/MessageThread.swift
@@ -0,0 +1,47 @@
+//
+//  MessageThread.swift
+//  iOS Message Board
+//
+//  Created by Jaspal on 1/23/19.
+//  Copyright © 2019 Jaspal Suri. All rights reserved.
+//
+
+import Foundation
+
+class MessageThread: Equatable, Codable {
+    
+    let title: String
+    let identifier: String
+    
+    // Not sure if struct or class would be better here.
+    struct Message: Equatable, Codable {
+        let text: String
+        let sender: String
+        let timeStamp: Date
+        
+        // Initializer in class: Simple
+        // Initializer in project: one more thing...
+        init(text: String, sender: String, timeStamp: Date = Date()) {
+            self.text = text
+            self.sender = sender
+            self.timeStamp = timeStamp
+        }
+    }
+    
+    var messages: [MessageThread.Message]
+    
+    init(title: String, identifier: String = UUID().uuidString, messages: [MessageThread.Message] = []) {
+        self.title = title
+        self.identifier = identifier
+        self.messages = messages
+    }
+    
+    // Make sure both sides are equal to each other.
+    // The identifier is what matters most here, since we need to retrieve
+    // the correct item's content and we're not using SHA or GPG verification.
+    static func ==(lhs: MessageThread, rhs: MessageThread) -> Bool {
+        return rhs.identifier == lhs.identifier &&
+            lhs.identifier == rhs.identifier
+    }
+}
+
