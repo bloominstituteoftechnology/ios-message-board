2d75cfb743a66bdf34dfc291b6c84805d6fb2083
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-01-23 21:00:15 -0500

    update layout and connection

---
+++ Message Board/Message Board/MessageThread.swift
@@ -0,0 +1,9 @@
+//
+//  MessageThread.swift
+//  Message Board
+//
+//  Created by Julian A. Fordyce on 1/23/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import Foundation
---
+++ Message Board/Message Board/MessageThreadController.swift
@@ -0,0 +1,9 @@
+//
+//  MessageThreadController.swift
+//  Message Board
+//
+//  Created by Julian A. Fordyce on 1/23/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import Foundation
