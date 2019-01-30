277317361710623fd7f132c55f15ebfae9b949c9
Paul Yi <yipaulc@gmail.com>
2019-01-30 15:46:13 -0800

    Create MessageThread class

---
+++ MessageBoard/MessageBoard/MessageThread.swift
@@ -0,0 +1,14 @@
+//
+//  MessageThread.swift
+//  MessageBoard
+//
+//  Created by Paul Yi on 1/30/19.
+//  Copyright © 2019 Paul Yi. All rights reserved.
+//
+
+import Foundation
+
+class MessageThread: Codable {
+    let title: String
+    let identifier: String
+}
