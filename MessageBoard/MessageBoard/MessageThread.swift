2ae83c5f22549ab92a5dfb9f46fae0b0f28689f2
Paul Yi <yipaulc@gmail.com>
2019-01-30 15:47:13 -0800

    Create Message struct

--- MessageBoard/MessageBoard/MessageThread.swift
+++ MessageBoard/MessageBoard/MessageThread.swift
@@ -11,4 +11,16 @@ import Foundation
 class MessageThread: Codable {
     let title: String
     let identifier: String
+    
+    struct Message: Equatable, Codable {
+        let text: String
+        let sender: String
+        let timeStamp: Date
+        
+        init(text: String, sender: String, timeStamp: Date = Date()) {
+            self.text = text
+            self.sender = sender
+            self.timeStamp = timeStamp
+        }
+    }
 }
