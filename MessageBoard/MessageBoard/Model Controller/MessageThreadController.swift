9fa4456b1f7f6b5e5deda0b9429e0a8b04efd294
Paul Yi <yipaulc@gmail.com>
2019-01-31 22:23:58 -0800

    Create a function called createMessageThread

---
+++ MessageBoard/MessageBoard/Model Controller/MessageThreadController.swift
@@ -0,0 +1,47 @@
+//
+//  MessageThreadController.swift
+//  MessageBoard
+//
+//  Created by Paul Yi on 1/30/19.
+//  Copyright © 2019 Paul Yi. All rights reserved.
+//
+
+import Foundation
+
+class MessageThreadController {
+    var messageThreads: [MessageThread] = []
+    
+    static let baseURL = URL(string: "https://lambda-message-board.firebaseio.com/")!
+    
+    func createMessageThread(title: String, completion: @escaping (_ success: Bool) -> Void) {
+        let messageThread = MessageThread(title: title)
+        let url = MessageThreadController.baseURL.appendingPathComponent(messageThread.identifier).appendingPathExtension("json")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(messageThread)
+        } catch {
+            fatalError("Error encoding message thread: \(messageThread) \(error)")
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error: \(error)")
+                completion(false)
+                return
+            }
+            
+            self.messageThreads.append(messageThread)
+            completion(true)
+        }.resume()
+    }
+    
+    
+}
+
+
+
+
+
