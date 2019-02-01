b8bf4992b39fb147e813fa3de5eaced0fc6cded2
Paul Yi <yipaulc@gmail.com>
2019-01-31 22:38:26 -0800

    Implement MessageThreadsTableViewController methods

--- MessageBoard/MessageBoard/View Controllers/MessageThreadsTableViewController.swift
+++ MessageBoard/MessageBoard/View Controllers/MessageThreadsTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class MessageThreadsTableViewController: UITableViewController {
+    let messageThreadController = MessageThreadController()
+    
     @IBOutlet weak var messageThreadTextField: UITextField!
     
     
@@ -24,66 +26,33 @@ class MessageThreadsTableViewController: UITableViewController {
     }
     
     @IBAction func returnMessageThread(_ sender: Any) {
+        guard let title = messageThreadTextField.text else { return }
+        
+        messageThreadController.createMessageThread(title: title) { (success) in
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        
+        messageThreadTextField.text = ""
     }
     
     
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return messageThreadController.messageThreads.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        // Configure the cell...
+        let message = messageThreadController.messageThreads[indexPath.row]
+        cell.textLabel?.text = message.title
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
