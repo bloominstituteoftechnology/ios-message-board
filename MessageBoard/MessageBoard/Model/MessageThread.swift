86adce1a028b86d0957ce8778e614ea5d7ecf42d
Paul Yi <yipaulc@gmail.com>
2019-01-31 23:12:54 -0800

    Add initializer to MessageThread class

--- MessageBoard/MessageBoard/Model/MessageThread.swift
+++ MessageBoard/MessageBoard/Model/MessageThread.swift
@@ -18,6 +18,26 @@ class MessageThread: Codable {
         self.identifier = identifier
         self.messages = messages
     }
+    
+    required init(from decoder: Decoder) throws {
+        
+        // 1
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        // 2
+        let title = try container.decode(String.self, forKey: .title)
+        let identifier = try container.decode(String.self, forKey: .identifier)
+        let messagesDictionaries = try container.decodeIfPresent([String: Message].self, forKey: .messages)
+        
+        // 3
+        let messages = messagesDictionaries?.compactMap({ $0.value }) ?? []
+        
+        // 4
+        self.title = title
+        self.identifier = identifier
+        self.messages = messages
+    }
+    
     // Adopt the Equatable protocol
     static func == (lhs: MessageThread, rhs: MessageThread) -> Bool {
         return lhs.title == rhs.title && lhs.identifier == rhs.identifier && lhs.messages == rhs.messages
