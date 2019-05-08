2e6f2ccb523d2f030b3f55366e54351a77bacfd7
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-08 18:05:27 -0400

    Add required init from decoder

--- Message Board/Message Board/Model/MessageThread.swift
+++ Message Board/Message Board/Model/MessageThread.swift
@@ -24,6 +24,21 @@ class MessageThread: Equatable, Codable {
         return lhs.title == rhs.title && lhs.identifier == rhs.identifier && lhs.messages == rhs.messages
     }
     
+    required init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let title = try container.decode(String.self, forKey: .title)
+        let identifier = try container.decode(String.self, forKey: .identifier)
+        let messagesDictionaries = try container.decodeIfPresent([String: Message].self, forKey: .messages)
+        
+        let messages = messagesDictionaries?.compactMap({ $0.value }) ?? []
+        
+        self.title = title
+        self.identifier = identifier
+        self.messages = messages
+    }
+    
     // MARK: - Message Struct
     
     struct Message: Equatable, Codable {
