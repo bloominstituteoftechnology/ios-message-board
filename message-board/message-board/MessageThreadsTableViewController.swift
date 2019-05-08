b88cc9eb636e44d2678a294c2f77c374016bec23
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-08 12:59:27 -0700

    TableViewController setup

---
+++ message-board/message-board/MessageThreadsTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MessageThreadsTableViewController.swift
+//  message-board
+//
+//  Created by Hector Steven on 5/8/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class MessageThreadsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+	}
+
+}
