ce14cd9304b3b66801debf136a89bb13df6e9a03
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-08 13:18:02 -0700

    MessageDetailViewController

---
+++ message-board/message-board/View Controllers/MessageDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MessageDetailViewController.swift
+//  message-board
+//
+//  Created by Hector Steven on 5/8/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
