3cb8f9c3f006bf518f6457a080e4aa6894ad5d52
Michael R.F <mrflowers22@uwalumni.com>
2019-05-08 17:24:14 -0400

    implemented the logic for the model class

---
+++ MessageBoardHW/MessageBoardHW/MessageThread.swift
@@ -0,0 +1,32 @@
+//
+//  MessageThread.swift
+//  MessageBoardHW
+//
+//  Created by Michael Flowers on 5/8/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+class MessageThread: Codable, Equatable {
+    
+    var title: String
+    var identifier: String
+    var messages: [MessageThread.Message]
+    
+    init(title: String, identifier: String = UUID().uuidString, messages: [MessageThread.Message] = []) {
+        self.title = title
+        self.identifier = identifier
+        self.messages = messages
+    }
+    
+    static func == (lhs: MessageThread, rhs: MessageThread) -> Bool {
+        return lhs.title == rhs.title && lhs.identifier == rhs.identifier && lhs.messages == rhs.messages
+    }
+    
+    struct Message: Codable, Equatable {
+        let text: String
+        let sender: String
+        let timestamp: Date
+    }
+}
