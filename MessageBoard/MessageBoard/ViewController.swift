30959581ccbe4c639674f3535f9845be851206a9
Jeffrey Carpenter <spaz926@me.com>
2019-05-08 14:08:56 -0500

    Create starter Xcode project

---
+++ MessageBoard/MessageBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessageBoard
+//
+//  Created by Jeffrey Carpenter on 5/8/19.
+//  Copyright © 2019 Jeffrey Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
