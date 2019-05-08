b4979fd1a5fb31ec1c65fa401a6c0f154965a7b7
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-08 16:57:47 -0400

    Set up UI

---
+++ Message Board/Message Board/Model/MessageThread.swift
@@ -0,0 +1,9 @@
+//
+//  MessageThread.swift
+//  Message Board
+//
+//  Created by Lisa Sampson on 5/8/19.
+//  Copyright © 2019 Lisa M Sampson. All rights reserved.
+//
+
+import Foundation
---
+++ Message Board/Message Board/View Controllers/MessageDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MessageDetailViewController.swift
+//  Message Board
+//
+//  Created by Lisa Sampson on 5/8/19.
+//  Copyright © 2019 Lisa M Sampson. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sendButtonTapped(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
