458668cfebaed9a8280d54454bfd812160ca6f9e
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-08 13:14:49 -0700

    rest apis

---
+++ message-board/message-board/View Controllers/MessageThreadDetailTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MessageThreadDetailTableViewController.swift
+//  message-board
+//
+//  Created by Hector Steven on 5/8/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class MessageThreadDetailTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+	}
+
+}
