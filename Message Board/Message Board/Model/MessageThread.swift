e8a545b26b2314a0cfdca01c02b6da880498f4b2
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-08 17:16:25 -0400

    Create the model firle

--- Message Board/Message Board/Model/MessageThread.swift
+++ Message Board/Message Board/Model/MessageThread.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+class MessageThread: Equatable, Codable {
+    
+    let title: String
+    let identifier: String
+    var messages: [MessageThread.Message]
+    
+    init(title: String, identifier: String = UUID().uuidString, messages: [MessageThread.Message] = []) {
+        self.title = title
+        self.identifier = identifier
+        self.messages = messages
+    }
+
+    static func == (lhs: MessageThread, rhs: MessageThread) -> Bool {
+        return lhs.title == rhs.title && lhs.identifier == rhs.identifier && lhs.messages == rhs.messages
+    }
+    
+    // MARK: - Message Struct
+    
+    struct Message: Equatable, Codable {
+        let text: String
+        let sender: String
+        let timestamp: Date
+        
+        init(text: String, sender: String, timestamp: Date = Date()) {
+            self.text = text
+            self.sender = sender
+            self.timestamp = timestamp
+        }
+    }
+}
