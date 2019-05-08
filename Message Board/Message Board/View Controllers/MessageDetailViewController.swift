2860a8fc4baba554a8b14a5898a48d9a9bf6df01
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-08 18:04:07 -0400

    Set up detail message view

--- Message Board/Message Board/View Controllers/MessageDetailViewController.swift
+++ Message Board/Message Board/View Controllers/MessageDetailViewController.swift
@@ -10,30 +10,26 @@ import UIKit
 
 class MessageDetailViewController: UIViewController {
 
+    // MARK: - Properties and Outlets
+    
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var textView: UITextView!
     
     var messageThread: MessageThread?
     var messageThreadController: MessageThreadController?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    // MARK: - IBActions
     
     @IBAction func sendButtonTapped(_ sender: Any) {
         
+        guard let name = textField.text,
+            let message = textView.text,
+            let thread = messageThread else { return }
+        
+        messageThreadController?.createMessage(thread: thread, text: message, sender: name, completion: { (success) in
+            DispatchQueue.main.async {
+                self.navigationController?.popViewController(animated: true)
+            }
+        })
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
