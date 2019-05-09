fafc86a741cdd6344cc931f5c5b8d1ff73893706
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-08 19:48:26 -0700

    MessageDetailViewController

--- message-board/message-board/View Controllers/MessageDetailViewController.swift
+++ message-board/message-board/View Controllers/MessageDetailViewController.swift
@@ -12,8 +12,30 @@ class MessageDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		
+		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(save))
     }
 	
+	@objc func save() {
+		guard let name = nameTextField.text,
+			let message = messageTextView.text,
+			let messeageThread = messageThread else { return }
+		
+		print(name, message)
+		
+		messageThreadController?.createMessage(messageThread: messeageThread, text: message, sender: name, completion: { (error) in
+			if let error = error {
+				print(error)
+			}
+			
+			DispatchQueue.main.async {
+				self.navigationController?.popToRootViewController(animated: true)
+			}
+		})
+		
+		nameTextField?.text = nil
+		messageTextView?.text = nil
+	}
 	
 	@IBOutlet var nameTextField: UITextField!
 	@IBOutlet var messageTextView: UITextView!
