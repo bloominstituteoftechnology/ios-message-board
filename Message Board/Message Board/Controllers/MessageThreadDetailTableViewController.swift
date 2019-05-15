704eb133a55445619aadc3c68c66de4359b1e078
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-05-15 18:06:41 -0400

    Setting up second tableview.

--- Message Board/Message Board/Controllers/MessageThreadDetailTableViewController.swift
+++ Message Board/Message Board/Controllers/MessageThreadDetailTableViewController.swift
@@ -17,35 +17,40 @@ class MessageThreadDetailTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        navigationItem.title = messageThread?.title ?? ""
+        //twice in the same project ... wow.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        // THANK YOU STACK!!!!!!
+        return messageThread?.messages.count ?? 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailCell", for: indexPath)
+        let message = messageThread?.messages[indexPath.row]
+        
+        cell.textLabel?.text = message?.text
+        cell.detailTextLabel?.text = message?.sender
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
