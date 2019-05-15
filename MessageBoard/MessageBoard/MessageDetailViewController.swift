99e18b36afcc1aa89a8437b282d07dbb6722c8d9
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-05-15 15:34:28 -0400

    Set up Storyboard and view controller files

---
+++ MessageBoard/MessageBoard/MessageDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MessageDetailViewController.swift
+//  MessageBoard
+//
+//  Created by Thomas Cacciatore on 5/15/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+    @IBAction func sendButtonTapped(_ sender: Any) {
+    }
+    
+    
+    @IBOutlet weak var messageDetailVCTextView: UITextView!
+    @IBOutlet weak var messageDetailVCTextField: UITextField!
+}
