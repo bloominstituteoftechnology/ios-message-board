d25df55344beeefa170ef947c3fd8245d12f0c46
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-05-15 16:31:53 -0400

    Through to setting up the MessageThreadController

--- Message Board/Message Board/MessageThread.swift
+++ Message Board/Message Board/MessageThread.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+class MessageThread: Codable, Equatable {
+    var title: String
+    var identifier: String
+    var messages: [MessageThread.Message] = []
+    struct Message: Codable, Equatable {
+        var text: String
+        var sender: String
+        var timestamp: Date
+        
+        init(text: String, sender: String, timestamp: Date) {
+            self.text = text
+            self.sender = sender
+            self.timestamp = Date()
+        }
+    }
+    
+    init(title: String, identifier: String = UUID().uuidString) {
+        self.title = title
+        self.identifier = identifier
+        self.messages = []
+    }
+    static func ==(lhs: MessageThread, rhs: MessageThread) -> Bool {
+        
+       return lhs.title == rhs.title && lhs.identifier == rhs.identifier && lhs.messages == rhs.messages
+    }
+}
