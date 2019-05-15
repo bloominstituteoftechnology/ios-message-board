c2e9854c03acd1fe75b5db6da201c0ce2111b4d4
Jeremy Taylor <jtaylor@jctaylor.org>
2019-05-15 14:31:43 -0500

    Storyboard setup done

---
+++ MessageBoard/MessageBoard/View Controllers/MessageDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MessageDetailViewController.swift
+//  MessageBoard
+//
+//  Created by Jeremy Taylor on 5/15/19.
+//  Copyright © 2019 Bytes-Random L.L.C. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var postTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func send(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
