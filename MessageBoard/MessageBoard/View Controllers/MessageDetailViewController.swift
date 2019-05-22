37a461445c6ac0070f4e34590b427cc18a40a663
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-05-22 15:33:11 -0400

    Setup StoryBoard and added corresponding View Controller files.

---
+++ MessageBoard/MessageBoard/View Controllers/MessageDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MessageDetailViewController.swift
+//  MessageBoard
+//
+//  Created by Thomas Cacciatore on 5/22/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func sendButtonTapped(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+    @IBOutlet weak var detailTextField: UITextField!
+    
+    @IBOutlet weak var detailTextView: UITextView!
+}
---
+++ MessageBoard/MessageBoard/View Controllers/MessageThreadsTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MessageThreadsTableViewController.swift
+//  MessageBoard
+//
+//  Created by Thomas Cacciatore on 5/22/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+import UIKit
+
+class MessageThreadsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+    
+    @IBAction func textFieldAction(_ sender: Any) {
+    }
+    
+    
+
+    @IBOutlet weak var messageThreadTextField: UITextField!
+    
+}
